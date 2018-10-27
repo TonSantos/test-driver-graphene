1e5dbd00cdb4cc726531d6e8a3b9f110e6f39502
Glairton Santos <glairtons@gmail.com>
2018-10-27 07:19:33 -0300

    update schema

--- schema.py
+++ schema.py
@@ -9,6 +9,8 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(schema.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Mutations
+    # as we begin to add more apps to our project
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
